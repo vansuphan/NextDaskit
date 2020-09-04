0dc2017adbe0b8bf2690a7542803387ec1c1e729
vansuphan <37709216+vansuphan@users.noreply.github.com>
2020-09-05 00:48:39 +0700

    update page login

--- pages/login.js
+++ pages/login.js
@@ -2,34 +2,12 @@ import MasterPage from "components/page/MasterPage";
 import resource from "plugins/assets/resource";
 import CONFIG from "web.config";
 import FullScreenLayout from "components/layout/FullscreenLayout";
-import {FormLogin} from "components/FormLogin";
+import {FormLogin} from "components/FormLogin/FormLogin";
 
 // context
 import MainContentProvider from "contexts/MainContentContext"
 import HeaderProvider from "contexts/HeaderContext";
 //
-
-const dataTest = [
-  {
-      title:"Hello guy",
-      textContent: "It's a broader card with text below as a natural lead-in to extra content.",
-      timer: 1, 
-  },
-  {
-    title: "Alphazap",
-    textContent: "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
-    timer: 65
-  }, {
-    title: "Voyatouch",
-    textContent: "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
-    timer: 59
-  }, {
-    title: "Lotlux",
-    textContent: "Fusce consequat. Nulla nisl. Nunc nisl.",
-    timer: 17
-  }
-]
-
 export async function getServerSideProps(context) {
   // const params = context.params;
   // const query = context.query;
@@ -60,7 +38,10 @@ export default function Home(props) {
       <MainContentProvider>
       <FullScreenLayout>
         <HeaderProvider>
-            <FormLogin> </FormLogin>
+            <FormLogin
+              nameForm="Sign up"
+              typeFrom=""
+            ></FormLogin>
         </HeaderProvider>
       </FullScreenLayout>
       </MainContentProvider>
