23de0eca631cbd9a9262eaa786e3ef88b44f2024
suphan <suphan@wearetopgroup.com>
2020-10-09 18:23:07 +0700

    nothing

--- components/button/ButtonChangeBgWhite.js
+++ components/button/ButtonChangeBgWhite.js
@@ -1,7 +1,9 @@
 import {useContext} from "react";
 import {MainContentContext} from "contexts/MainContentContext";
 export default function ButtonChangeBg({ children, onClick }) {
+  
     const contexts = useContext(MainContentContext)
+    console.log("Context",contexts)
   return (
     <button onClick={contexts.setBgWhite}>
       Background White
